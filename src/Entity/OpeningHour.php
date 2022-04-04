22a536011c3e8eb0f96710575590b8e4d95d0246
pjehan <pierre.jehan@gmail.com>
2022-04-04 16:47:06 +0200

    Création de l'entité OpeningHour

---
+++ src/Entity/OpeningHour.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OpeningHourRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OpeningHourRepository::class)]
+class OpeningHour
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $weekDay;
+
+    #[ORM\Column(type: 'integer')]
+    private $weekNumber;
+
+    #[ORM\Column(type: 'time', nullable: true)]
+    private $openingTime;
+
+    #[ORM\Column(type: 'time', nullable: true)]
+    private $closingTime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWeekDay(): ?string
+    {
+        return $this->weekDay;
+    }
+
+    public function setWeekDay(string $weekDay): self
+    {
+        $this->weekDay = $weekDay;
+
+        return $this;
+    }
+
+    public function getWeekNumber(): ?int
+    {
+        return $this->weekNumber;
+    }
+
+    public function setWeekNumber(int $weekNumber): self
+    {
+        $this->weekNumber = $weekNumber;
+
+        return $this;
+    }
+
+    public function getOpeningTime(): ?\DateTimeInterface
+    {
+        return $this->openingTime;
+    }
+
+    public function setOpeningTime(?\DateTimeInterface $openingTime): self
+    {
+        $this->openingTime = $openingTime;
+
+        return $this;
+    }
+
+    public function getClosingTime(): ?\DateTimeInterface
+    {
+        return $this->closingTime;
+    }
+
+    public function setClosingTime(?\DateTimeInterface $closingTime): self
+    {
+        $this->closingTime = $closingTime;
+
+        return $this;
+    }
+}
