44e5d987671dd5040997d3cf52aa3962557adc57
pjehan <pierre.jehan@gmail.com>
2022-04-07 11:20:27 +0200

    Génération du CRUD pour l'entité Appointment

---
+++ src/Form/AppointmentType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Appointment;
+use App\Entity\Doctor;
+use App\Entity\Speciality;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AppointmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+                'attr' => [
+                    'min' => (new \DateTime())->format('Y-m-d\TH:i')
+                ]
+            ])
+            ->add('message')
+            ->add('doctor', EntityType::class, [
+                'class' => Doctor::class,
+                'choice_label' => 'fullName'
+            ])
+            ->add('speciality', EntityType::class, [
+                'class' => Speciality::class,
+                'choice_label' => 'name'
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'email'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Appointment::class,
+        ]);
+    }
+}
